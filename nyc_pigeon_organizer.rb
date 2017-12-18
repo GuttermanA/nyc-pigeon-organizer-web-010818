c62678fc406171db4258953d2b930743f3bde29d
Alexander Gutterman <alexanderf.gutterman@gmail.com>
2017-12-18 21:27:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pidgeon_list = {}
+  pidgeon_name_array = []
+  data.each do |info_label, info_data|
+    info_data.each do |key, value_array|
+      value_array.each do |value|
+        pidgeon_name_array << value
+        pidgeon_list[value] = {}
+      end
+
+    end
+  end
+  pidgeon_name_array.uniq!
+
+  pidgeon_name_array.each do |name|
+    data.each do |info_label, info_data|
+      pidgeon_list[name][info_label] = []
+      info_data.each do |key, value_array|
+        if value_array.include?(name) == true
+          pidgeon_list[name][info_label] << key.to_s
+        end
+      end
+    end
+  end
+  pidgeon_list
 end
